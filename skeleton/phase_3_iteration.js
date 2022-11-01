9ff11b498cf2db7a51b2715bb050b287daabdbaf
Brian Yu <briehnyu@gmail.com>
2022-11-01 16:31:07 -0400

    Better subStrings

--- skeleton/phase_3_iteration.js
+++ skeleton/phase_3_iteration.js
@@ -19,12 +19,10 @@ Array.prototype.bubbleSort = function() {
 
 String.prototype.subStrings = function() {
     let subStrings = [];
-    for (let i = 0; i < this.length - 1; i++) {
-        for (let j = i + 1; j < this.length; j++) {
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length + 1; j++) {
             subStrings.push(this.slice(i, j));
         }
     }
-    subStrings.push(this[this.length-1]);
-    subStrings.push(this.toString());
     return subStrings;
 }
